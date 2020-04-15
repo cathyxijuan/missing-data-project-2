a6de0bc3c4d7b6d0afec04dc856d3cbe89104b64
cathyxijuan <cathyxijuan@gmail.com>
2020-04-15 07:12:31 -0700

    update

--- Simu results out MI.R
+++ Simu results out MI.R
@@ -1,5 +1,7 @@
+setwd("/Volumes/SP PHD U3/missing-data-project-2/Simu results MI")
+
 load("fitMAR_Strong_20PerMiss_2VarMiss_1CR_DF_MI_n1000.RData")
 load("fitMAR_Strong_50PerMiss_2VarMiss_1CR_DF_MI_n1000.RData")
-fitMAR_Strong_20PerMiss_2VarMiss_1CR_DF_MI_n1000[[1]]
-fitMAR_Strong_50PerMiss_2VarMiss_1CR_DF_MI_n1000[[1]]
+
+
 
