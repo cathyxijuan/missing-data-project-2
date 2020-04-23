b4000cc7c1fae1a6a75fd13613dbf2a44b3fcf0c
cathyxijuan <cathyxijuan@gmail.com>
2020-04-23 05:16:56 -0700

    update

--- Simu results MI/load MI results.R
+++ Simu results MI/load MI results.R
@@ -15,19 +15,19 @@ load("fitMAR_Strong_50PerMiss_2VarMiss_1CR_DF_MI_n1000.RData")
 load("fitMCAR_20PerMiss_2VarMiss_1CR_DF_MI_n200.RData")
 load("fitMCAR_20PerMiss_2VarMiss_1CR_DF_MI_n500.RData")
 load("fitMCAR_20PerMiss_2VarMiss_1CR_DF_MI_n1000.RData")
-#load("fitMCAR_20PerMiss_2VarMiss_1CR_DF_MI_n1000000.RData")
+load("fitMCAR_20PerMiss_2VarMiss_1CR_DF_MI_n1000000.RData")
 load("fitMCAR_50PerMiss_2VarMiss_1CR_DF_MI_n200.RData")
 load("fitMCAR_50PerMiss_2VarMiss_1CR_DF_MI_n500.RData")
 load("fitMCAR_50PerMiss_2VarMiss_1CR_DF_MI_n1000.RData")
-#load("fitMCAR_50PerMiss_2VarMiss_1CR_DF_MI_n1000000.RData")
+load("fitMCAR_50PerMiss_2VarMiss_1CR_DF_MI_n1000000.RData")
 load("fitMCAR_20PerMiss_4VarMiss_1CR_DF_MI_n200.RData")
 load("fitMCAR_20PerMiss_4VarMiss_1CR_DF_MI_n500.RData")
 load("fitMCAR_20PerMiss_4VarMiss_1CR_DF_MI_n1000.RData")
-#load("fitMCAR_20PerMiss_4VarMiss_1CR_DF_MI_n1000000.RData")
+load("fitMCAR_20PerMiss_4VarMiss_1CR_DF_MI_n1000000.RData")
 load("fitMCAR_50PerMiss_4VarMiss_1CR_DF_MI_n200.RData")
 load("fitMCAR_50PerMiss_4VarMiss_1CR_DF_MI_n500.RData")
 load("fitMCAR_50PerMiss_4VarMiss_1CR_DF_MI_n1000.RData")
-#load("fitMCAR_50PerMiss_4VarMiss_1CR_DF_MI_n1000000.RData")
+load("fitMCAR_50PerMiss_4VarMiss_1CR_DF_MI_n1000000.RData")
 load("fitMCAR_20PerMiss_2VarMiss_2CR_DF_MI_n200.RData")
 load("fitMCAR_20PerMiss_2VarMiss_2CR_DF_MI_n500.RData")
 load("fitMCAR_20PerMiss_2VarMiss_2CR_DF_MI_n1000.RData")
@@ -76,3 +76,22 @@ load("fitMCAR_50PerMiss_4VarMiss_2CR_SF_MI_n200.RData")
 load("fitMCAR_50PerMiss_4VarMiss_2CR_SF_MI_n500.RData")
 load("fitMCAR_50PerMiss_4VarMiss_2CR_SF_MI_n1000.RData")
 load("fitMCAR_50PerMiss_4VarMiss_2CR_SF_MI_n1000000.RData")
+load("/Volumes/SP PHD U3/missing-data-project-2/Simu results MI/fitMAR_Strong_minPat_50PerMiss_4VarMiss_WM_MI_n200.RData")
+load("/Volumes/SP PHD U3/missing-data-project-2/Simu results MI/fitMAR_Strong_minPat_50PerMiss_4VarMiss_WM_MI_n500.RData")
+
+
+load("/Volumes/SP PHD U3/missing-data-project-2/Simu results MI/fitMAR_Strong_20PerMiss_2VarMiss_1CR_DF_MI_n200.RData")
+load("/Volumes/SP PHD U3/missing-data-project-2/Simu results MI/fitMAR_Strong_20PerMiss_2VarMiss_1CR_DF_MI_n500.RData")
+load("/Volumes/SP PHD U3/missing-data-project-2/Simu results MI/fitMAR_Strong_20PerMiss_2VarMiss_1CR_DF_MI_n500.RData")
+
+
+fitMAR_Strong_20PerMiss_2VarMiss_1CR_DF_MI_n200[[1]][1:2,]
+fitMAR_Strong_20PerMiss_2VarMiss_1CR_DF_MI_n200[[2]][1:2,]
+
+fitMAR_Strong_minPat_50PerMiss_4VarMiss_WM_MI_n200[[1]][1:2,]
+fitMAR_Strong_minPat_50PerMiss_4VarMiss_WM_MI_n200[[2]][1:2,]
+
+fitMAR_Strong_minPat_50PerMiss_4VarMiss_WM_MI_n500[[1]][1:2,]
+fitMAR_Strong_minPat_50PerMiss_4VarMiss_WM_MI_n500[[2]][1:2,]
+fitMAR_Strong_minPat_50PerMiss_4VarMiss_WM_MI_n500[[3]][1:2,]
+
