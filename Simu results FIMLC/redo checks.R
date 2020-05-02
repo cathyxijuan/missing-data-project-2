76009ffdc6f0fcedc514d2b48454ef954f0b4304
cathyxijuan <cathyxijuan@gmail.com>
2020-05-02 16:30:03 -0700

    update

--- Simu results FIMLC/redo checks.R
+++ Simu results FIMLC/redo checks.R
@@ -314,3 +314,437 @@ save(fitMCAR_20PerMiss_2VarMiss_1CR_DF_fimlc_checks_n1000,file="fitMCAR_20PerMis
 save(fitMCAR_50PerMiss_2VarMiss_1CR_DF_fimlc_checks_n1000,file="fitMCAR_50PerMiss_2VarMiss_1CR_DF_fimlc_checks_n1000.RData")
 save(fitMCAR_20PerMiss_4VarMiss_1CR_DF_fimlc_checks_n1000,file="fitMCAR_20PerMiss_4VarMiss_1CR_DF_fimlc_checks_n1000.RData")
 save(fitMCAR_50PerMiss_4VarMiss_1CR_DF_fimlc_checks_n1000,file="fitMCAR_50PerMiss_4VarMiss_1CR_DF_fimlc_checks_n1000.RData")
+
+
+
+
+##### another conditions 
+
+
+
+
+
+
+load( file="fitMCAR_0PerMiss_WM_fimlc_compo_n200.RData")
+load( file="fitMCAR_0PerMiss_WM_fimlc_n200.RData")
+fitMCAR_0PerMiss_WM_fimlc_checks_n200<-fimlc.checks(fitMCAR_0PerMiss_WM_fimlc_compo_n200,
+                                                    fitMCAR_0PerMiss_WM_fimlc_n200)
+save(fitMCAR_0PerMiss_WM_fimlc_checks_n200, file="fitMCAR_0PerMiss_WM_fimlc_checks_n200.RData")
+
+
+
+load(file="fitMCAR_MinPat_20PerMiss_2VarMiss_WM_fimlc_compo_n200.RData")
+load(file="fitMCAR_MinPat_20PerMiss_2VarMiss_WM_fimlc_n200.RData")
+fitMCAR_MinPat_20PerMiss_2VarMiss_WM_fimlc_checks_n200<- 
+  fimlc.checks(fitMCAR_MinPat_20PerMiss_2VarMiss_WM_fimlc_compo_n200,
+               fitMCAR_MinPat_20PerMiss_2VarMiss_WM_fimlc_n200)
+
+save(fitMCAR_MinPat_20PerMiss_2VarMiss_WM_fimlc_checks_n200, file="fitMCAR_MinPat_20PerMiss_2VarMiss_WM_fimlc_checks_n200.RData")
+
+
+
+
+
+load( file="fitMCAR_MinPat_50PerMiss_2VarMiss_WM_fimlc_compo_n200.RData")
+load( file="fitMCAR_MinPat_50PerMiss_2VarMiss_WM_fimlc_n200.RData")
+fitMCAR_MinPat_50PerMiss_2VarMiss_WM_fimlc_checks_n200<- 
+  fimlc.checks(fitMCAR_MinPat_50PerMiss_2VarMiss_WM_fimlc_compo_n200,
+               fitMCAR_MinPat_50PerMiss_2VarMiss_WM_fimlc_n200)
+
+save(fitMCAR_MinPat_50PerMiss_2VarMiss_WM_fimlc_checks_n200, file="fitMCAR_MinPat_50PerMiss_2VarMiss_WM_fimlc_checks_n200.RData")
+
+
+
+
+
+load( file="fitMCAR_MinPat_20PerMiss_4VarMiss_WM_fimlc_n200.RData")
+load( file="fitMCAR_MinPat_20PerMiss_4VarMiss_WM_fimlc_compo_n200.RData")
+fitMCAR_MinPat_20PerMiss_4VarMiss_WM_fimlc_checks_n200<- 
+  fimlc.checks(fitMCAR_MinPat_20PerMiss_4VarMiss_WM_fimlc_compo_n200,
+               fitMCAR_MinPat_20PerMiss_4VarMiss_WM_fimlc_n200)
+
+save(fitMCAR_MinPat_20PerMiss_4VarMiss_WM_fimlc_checks_n200, file="fitMCAR_MinPat_20PerMiss_4VarMiss_WM_fimlc_checks_n200.RData")
+
+
+
+
+load( file="fitMCAR_MinPat_50PerMiss_4VarMiss_WM_fimlc_compo_n200.RData")
+load(file="fitMCAR_MinPat_50PerMiss_4VarMiss_WM_fimlc_n200.RData")
+fitMCAR_MinPat_50PerMiss_4VarMiss_WM_fimlc_checks_n200<- 
+  fimlc.checks(fitMCAR_MinPat_50PerMiss_4VarMiss_WM_fimlc_compo_n200,
+               fitMCAR_MinPat_50PerMiss_4VarMiss_WM_fimlc_n200)
+
+
+save(fitMCAR_MinPat_50PerMiss_4VarMiss_WM_fimlc_checks_n200, file="fitMCAR_MinPat_50PerMiss_4VarMiss_WM_fimlc_checks_n200.RData")
+
+
+
+
+
+load( file="fitMCAR_MinPat_20PerMiss_6VarMiss_WM_fimlc_compo_n200.RData")
+load( file="fitMCAR_MinPat_20PerMiss_6VarMiss_WM_fimlc_n200.RData")
+fitMCAR_MinPat_20PerMiss_6VarMiss_WM_fimlc_checks_n200<- 
+  fimlc.checks(fitMCAR_MinPat_20PerMiss_6VarMiss_WM_fimlc_compo_n200,
+               fitMCAR_MinPat_20PerMiss_6VarMiss_WM_fimlc_n200)
+
+save(fitMCAR_MinPat_20PerMiss_6VarMiss_WM_fimlc_checks_n200, file="fitMCAR_MinPat_20PerMiss_6VarMiss_WM_fimlc_checks_n200.RData")
+
+
+
+
+
+load(file="fitMCAR_MinPat_50PerMiss_6VarMiss_WM_fimlc_compo_n200.RData")
+load(file="fitMCAR_MinPat_50PerMiss_6VarMiss_WM_fimlc_n200.RData")
+fitMCAR_MinPat_50PerMiss_6VarMiss_WM_fimlc_checks_n200<- 
+  fimlc.checks(fitMCAR_MinPat_50PerMiss_6VarMiss_WM_fimlc_compo_n200,
+               fitMCAR_MinPat_50PerMiss_6VarMiss_WM_fimlc_n200)
+
+
+save(fitMCAR_MinPat_50PerMiss_6VarMiss_WM_fimlc_checks_n200, file="fitMCAR_MinPat_50PerMiss_6VarMiss_WM_fimlc_checks_n200.RData")
+
+
+load(file="fitMCAR_MaxPat_20PerMiss_2VarMiss_WM_fimlc_compo_n200.RData")
+load(file="fitMCAR_MaxPat_20PerMiss_2VarMiss_WM_fimlc_n200.RData")
+fitMCAR_MaxPat_20PerMiss_2VarMiss_WM_fimlc_checks_n200<- 
+  fimlc.checks(fitMCAR_MaxPat_20PerMiss_2VarMiss_WM_fimlc_compo_n200,
+               fitMCAR_MaxPat_20PerMiss_2VarMiss_WM_fimlc_n200)
+
+save(fitMCAR_MaxPat_20PerMiss_2VarMiss_WM_fimlc_checks_n200, file="fitMCAR_MaxPat_20PerMiss_2VarMiss_WM_fimlc_checks_n200.RData")
+
+
+
+load( file="fitMCAR_MaxPat_50PerMiss_2VarMiss_WM_fimlc_compo_n200.RData")
+load( file="fitMCAR_MaxPat_50PerMiss_2VarMiss_WM_fimlc_n200.RData")
+fitMCAR_MaxPat_50PerMiss_2VarMiss_WM_fimlc_checks_n200<- 
+  fimlc.checks(fitMCAR_MaxPat_50PerMiss_2VarMiss_WM_fimlc_compo_n200,fitMCAR_MaxPat_50PerMiss_2VarMiss_WM_fimlc_n200 )
+
+save(fitMCAR_MaxPat_50PerMiss_2VarMiss_WM_fimlc_checks_n200, file="fitMCAR_MaxPat_50PerMiss_2VarMiss_WM_fimlc_checks_n200.RData")
+
+
+
+
+load( file="fitMCAR_MaxPat_20PerMiss_4VarMiss_WM_fimlc_compo_n200.RData")
+load( file="fitMCAR_MaxPat_20PerMiss_4VarMiss_WM_fimlc_n200.RData")
+fitMCAR_MaxPat_20PerMiss_4VarMiss_WM_fimlc_checks_n200<- 
+  fimlc.checks(fitMCAR_MaxPat_20PerMiss_4VarMiss_WM_fimlc_compo_n200,
+               fitMCAR_MaxPat_20PerMiss_4VarMiss_WM_fimlc_n200)
+
+
+save(fitMCAR_MaxPat_20PerMiss_4VarMiss_WM_fimlc_checks_n200, file="fitMCAR_MaxPat_20PerMiss_4VarMiss_WM_fimlc_checks_n200.RData")
+
+
+load(file="fitMCAR_MaxPat_50PerMiss_4VarMiss_WM_fimlc_compo_n200.RData")
+load( file="fitMCAR_MaxPat_50PerMiss_4VarMiss_WM_fimlc_n200.RData")
+fitMCAR_MaxPat_50PerMiss_4VarMiss_WM_fimlc_checks_n200<-
+  fimlc.checks(fitMCAR_MaxPat_50PerMiss_4VarMiss_WM_fimlc_compo_n200,
+               fitMCAR_MaxPat_50PerMiss_4VarMiss_WM_fimlc_n200)
+
+save(fitMCAR_MaxPat_50PerMiss_4VarMiss_WM_fimlc_checks_n200, file="fitMCAR_MaxPat_50PerMiss_4VarMiss_WM_fimlc_checks_n200.RData")
+
+
+
+
+load( file="fitMCAR_MaxPat_20PerMiss_6VarMiss_WM_fimlc_compo_n200.RData")
+load( file="fitMCAR_MaxPat_20PerMiss_6VarMiss_WM_fimlc_n200.RData")
+fitMCAR_MaxPat_20PerMiss_6VarMiss_WM_fimlc_checks_n200<- 
+  fimlc.checks(fitMCAR_MaxPat_20PerMiss_6VarMiss_WM_fimlc_compo_n200,
+               fitMCAR_MaxPat_20PerMiss_6VarMiss_WM_fimlc_n200)
+
+save(fitMCAR_MaxPat_20PerMiss_6VarMiss_WM_fimlc_checks_n200, file="fitMCAR_MaxPat_20PerMiss_6VarMiss_WM_fimlc_checks_n200.RData")
+
+
+
+
+load(file="fitMCAR_MaxPat_50PerMiss_6VarMiss_WM_fimlc_compo_n200.RData")
+load(file="fitMCAR_MaxPat_50PerMiss_6VarMiss_WM_fimlc_n200.RData")
+fitMCAR_MaxPat_50PerMiss_6VarMiss_WM_fimlc_checks_n200<- 
+  fimlc.checks(fitMCAR_MaxPat_50PerMiss_6VarMiss_WM_fimlc_compo_n200,
+               fitMCAR_MaxPat_50PerMiss_6VarMiss_WM_fimlc_n200)
+
+save(fitMCAR_MaxPat_50PerMiss_6VarMiss_WM_fimlc_checks_n200, file="fitMCAR_MaxPat_50PerMiss_6VarMiss_WM_fimlc_checks_n200.RData")
+
+
+
+
+load( file="fitMCAR_0PerMiss_WM_fimlc_compo_n500.RData")
+load( file="fitMCAR_0PerMiss_WM_fimlc_n500.RData")
+fitMCAR_0PerMiss_WM_fimlc_checks_n500<-fimlc.checks(fitMCAR_0PerMiss_WM_fimlc_compo_n500,
+                                                    fitMCAR_0PerMiss_WM_fimlc_n500)
+save(fitMCAR_0PerMiss_WM_fimlc_checks_n500, file="fitMCAR_0PerMiss_WM_fimlc_checks_n500.RData")
+
+
+
+load(file="fitMCAR_MinPat_20PerMiss_2VarMiss_WM_fimlc_compo_n500.RData")
+load(file="fitMCAR_MinPat_20PerMiss_2VarMiss_WM_fimlc_n500.RData")
+fitMCAR_MinPat_20PerMiss_2VarMiss_WM_fimlc_checks_n500<- 
+  fimlc.checks(fitMCAR_MinPat_20PerMiss_2VarMiss_WM_fimlc_compo_n500,
+               fitMCAR_MinPat_20PerMiss_2VarMiss_WM_fimlc_n500)
+
+save(fitMCAR_MinPat_20PerMiss_2VarMiss_WM_fimlc_checks_n500, file="fitMCAR_MinPat_20PerMiss_2VarMiss_WM_fimlc_checks_n500.RData")
+
+
+
+
+
+load( file="fitMCAR_MinPat_50PerMiss_2VarMiss_WM_fimlc_compo_n500.RData")
+load( file="fitMCAR_MinPat_50PerMiss_2VarMiss_WM_fimlc_n500.RData")
+fitMCAR_MinPat_50PerMiss_2VarMiss_WM_fimlc_checks_n500<- 
+  fimlc.checks(fitMCAR_MinPat_50PerMiss_2VarMiss_WM_fimlc_compo_n500,
+               fitMCAR_MinPat_50PerMiss_2VarMiss_WM_fimlc_n500)
+
+save(fitMCAR_MinPat_50PerMiss_2VarMiss_WM_fimlc_checks_n500, file="fitMCAR_MinPat_50PerMiss_2VarMiss_WM_fimlc_checks_n500.RData")
+
+
+
+
+
+
+load( file="fitMCAR_MinPat_20PerMiss_4VarMiss_WM_fimlc_n500.RData")
+load( file="fitMCAR_MinPat_20PerMiss_4VarMiss_WM_fimlc_compo_n500.RData")
+fitMCAR_MinPat_20PerMiss_4VarMiss_WM_fimlc_checks_n500<- 
+  fimlc.checks(fitMCAR_MinPat_20PerMiss_4VarMiss_WM_fimlc_compo_n500,
+               fitMCAR_MinPat_20PerMiss_4VarMiss_WM_fimlc_n500)
+
+save(fitMCAR_MinPat_20PerMiss_4VarMiss_WM_fimlc_checks_n500, file="fitMCAR_MinPat_20PerMiss_4VarMiss_WM_fimlc_checks_n500.RData")
+
+
+
+load( file="fitMCAR_MinPat_50PerMiss_4VarMiss_WM_fimlc_compo_n500.RData")
+load(file="fitMCAR_MinPat_50PerMiss_4VarMiss_WM_fimlc_n500.RData")
+fitMCAR_MinPat_50PerMiss_4VarMiss_WM_fimlc_checks_n500<- 
+  fimlc.checks(fitMCAR_MinPat_50PerMiss_4VarMiss_WM_fimlc_compo_n500,
+               fitMCAR_MinPat_50PerMiss_4VarMiss_WM_fimlc_n500)
+
+
+save(fitMCAR_MinPat_50PerMiss_4VarMiss_WM_fimlc_checks_n500, file="fitMCAR_MinPat_50PerMiss_4VarMiss_WM_fimlc_checks_n500.RData")
+
+
+
+
+
+load( file="fitMCAR_MinPat_20PerMiss_6VarMiss_WM_fimlc_compo_n500.RData")
+load( file="fitMCAR_MinPat_20PerMiss_6VarMiss_WM_fimlc_n500.RData")
+fitMCAR_MinPat_20PerMiss_6VarMiss_WM_fimlc_checks_n500<- 
+  fimlc.checks(fitMCAR_MinPat_20PerMiss_6VarMiss_WM_fimlc_compo_n500,
+               fitMCAR_MinPat_20PerMiss_6VarMiss_WM_fimlc_n500)
+
+save(fitMCAR_MinPat_20PerMiss_6VarMiss_WM_fimlc_checks_n500, file="fitMCAR_MinPat_20PerMiss_6VarMiss_WM_fimlc_checks_n500.RData")
+
+
+
+
+
+load(file="fitMCAR_MinPat_50PerMiss_6VarMiss_WM_fimlc_compo_n500.RData")
+load(file="fitMCAR_MinPat_50PerMiss_6VarMiss_WM_fimlc_n500.RData")
+fitMCAR_MinPat_50PerMiss_6VarMiss_WM_fimlc_checks_n500<- 
+  fimlc.checks(fitMCAR_MinPat_50PerMiss_6VarMiss_WM_fimlc_compo_n500,
+               fitMCAR_MinPat_50PerMiss_6VarMiss_WM_fimlc_n500)
+
+
+save(fitMCAR_MinPat_50PerMiss_6VarMiss_WM_fimlc_checks_n500, file="fitMCAR_MinPat_50PerMiss_6VarMiss_WM_fimlc_checks_n500.RData")
+
+
+load(file="fitMCAR_MaxPat_20PerMiss_2VarMiss_WM_fimlc_compo_n500.RData")
+load(file="fitMCAR_MaxPat_20PerMiss_2VarMiss_WM_fimlc_n500.RData")
+fitMCAR_MaxPat_20PerMiss_2VarMiss_WM_fimlc_checks_n500<- 
+  fimlc.checks(fitMCAR_MaxPat_20PerMiss_2VarMiss_WM_fimlc_compo_n500,
+               fitMCAR_MaxPat_20PerMiss_2VarMiss_WM_fimlc_n500)
+
+save(fitMCAR_MaxPat_20PerMiss_2VarMiss_WM_fimlc_checks_n500, file="fitMCAR_MaxPat_20PerMiss_2VarMiss_WM_fimlc_checks_n500.RData")
+
+
+
+load( file="fitMCAR_MaxPat_50PerMiss_2VarMiss_WM_fimlc_compo_n500.RData")
+load( file="fitMCAR_MaxPat_50PerMiss_2VarMiss_WM_fimlc_n500.RData")
+fitMCAR_MaxPat_50PerMiss_2VarMiss_WM_fimlc_checks_n500<- 
+  fimlc.checks(fitMCAR_MaxPat_50PerMiss_2VarMiss_WM_fimlc_compo_n500,fitMCAR_MaxPat_50PerMiss_2VarMiss_WM_fimlc_n500 )
+
+save(fitMCAR_MaxPat_50PerMiss_2VarMiss_WM_fimlc_checks_n500, file="fitMCAR_MaxPat_50PerMiss_2VarMiss_WM_fimlc_checks_n500.RData")
+
+
+
+
+load( file="fitMCAR_MaxPat_20PerMiss_4VarMiss_WM_fimlc_compo_n500.RData")
+load( file="fitMCAR_MaxPat_20PerMiss_4VarMiss_WM_fimlc_n500.RData")
+fitMCAR_MaxPat_20PerMiss_4VarMiss_WM_fimlc_checks_n500<- 
+  fimlc.checks(fitMCAR_MaxPat_20PerMiss_4VarMiss_WM_fimlc_compo_n500,
+               fitMCAR_MaxPat_20PerMiss_4VarMiss_WM_fimlc_n500)
+
+
+save(fitMCAR_MaxPat_20PerMiss_4VarMiss_WM_fimlc_checks_n500, file="fitMCAR_MaxPat_20PerMiss_4VarMiss_WM_fimlc_checks_n500.RData")
+
+
+load(file="fitMCAR_MaxPat_50PerMiss_4VarMiss_WM_fimlc_compo_n500.RData")
+load( file="fitMCAR_MaxPat_50PerMiss_4VarMiss_WM_fimlc_n500.RData")
+fitMCAR_MaxPat_50PerMiss_4VarMiss_WM_fimlc_checks_n500<-
+  fimlc.checks(fitMCAR_MaxPat_50PerMiss_4VarMiss_WM_fimlc_compo_n500,
+               fitMCAR_MaxPat_50PerMiss_4VarMiss_WM_fimlc_n500)
+
+save(fitMCAR_MaxPat_50PerMiss_4VarMiss_WM_fimlc_checks_n500, file="fitMCAR_MaxPat_50PerMiss_4VarMiss_WM_fimlc_checks_n500.RData")
+
+
+
+
+load( file="fitMCAR_MaxPat_20PerMiss_6VarMiss_WM_fimlc_compo_n500.RData")
+load( file="fitMCAR_MaxPat_20PerMiss_6VarMiss_WM_fimlc_n500.RData")
+fitMCAR_MaxPat_20PerMiss_6VarMiss_WM_fimlc_checks_n500<- 
+  fimlc.checks(fitMCAR_MaxPat_20PerMiss_6VarMiss_WM_fimlc_compo_n500,
+               fitMCAR_MaxPat_20PerMiss_6VarMiss_WM_fimlc_n500)
+
+save(fitMCAR_MaxPat_20PerMiss_6VarMiss_WM_fimlc_checks_n500, file="fitMCAR_MaxPat_20PerMiss_6VarMiss_WM_fimlc_checks_n500.RData")
+
+
+
+
+load(file="fitMCAR_MaxPat_50PerMiss_6VarMiss_WM_fimlc_compo_n500.RData")
+load(file="fitMCAR_MaxPat_50PerMiss_6VarMiss_WM_fimlc_n500.RData")
+fitMCAR_MaxPat_50PerMiss_6VarMiss_WM_fimlc_checks_n500<- 
+  fimlc.checks(fitMCAR_MaxPat_50PerMiss_6VarMiss_WM_fimlc_compo_n500,
+               fitMCAR_MaxPat_50PerMiss_6VarMiss_WM_fimlc_n500)
+
+save(fitMCAR_MaxPat_50PerMiss_6VarMiss_WM_fimlc_checks_n500, file="fitMCAR_MaxPat_50PerMiss_6VarMiss_WM_fimlc_checks_n500.RData")
+
+
+
+
+
+load( file="fitMCAR_0PerMiss_WM_fimlc_compo_n1000.RData")
+load( file="fitMCAR_0PerMiss_WM_fimlc_n1000.RData")
+fitMCAR_0PerMiss_WM_fimlc_checks_n1000<-fimlc.checks(fitMCAR_0PerMiss_WM_fimlc_compo_n1000,
+                                                     fitMCAR_0PerMiss_WM_fimlc_n1000)
+save(fitMCAR_0PerMiss_WM_fimlc_checks_n1000, file="fitMCAR_0PerMiss_WM_fimlc_checks_n1000.RData")
+
+
+
+load(file="fitMCAR_MinPat_20PerMiss_2VarMiss_WM_fimlc_compo_n1000.RData")
+load(file="fitMCAR_MinPat_20PerMiss_2VarMiss_WM_fimlc_n1000.RData")
+fitMCAR_MinPat_20PerMiss_2VarMiss_WM_fimlc_checks_n1000<- 
+  fimlc.checks(fitMCAR_MinPat_20PerMiss_2VarMiss_WM_fimlc_compo_n1000,
+               fitMCAR_MinPat_20PerMiss_2VarMiss_WM_fimlc_n1000)
+
+save(fitMCAR_MinPat_20PerMiss_2VarMiss_WM_fimlc_checks_n1000, file="fitMCAR_MinPat_20PerMiss_2VarMiss_WM_fimlc_checks_n1000.RData")
+
+
+
+
+
+load( file="fitMCAR_MinPat_50PerMiss_2VarMiss_WM_fimlc_compo_n1000.RData")
+load( file="fitMCAR_MinPat_50PerMiss_2VarMiss_WM_fimlc_n1000.RData")
+fitMCAR_MinPat_50PerMiss_2VarMiss_WM_fimlc_checks_n1000<- 
+  fimlc.checks(fitMCAR_MinPat_50PerMiss_2VarMiss_WM_fimlc_compo_n1000,
+               fitMCAR_MinPat_50PerMiss_2VarMiss_WM_fimlc_n1000)
+
+save(fitMCAR_MinPat_50PerMiss_2VarMiss_WM_fimlc_checks_n1000, file="fitMCAR_MinPat_50PerMiss_2VarMiss_WM_fimlc_checks_n1000.RData")
+
+
+
+
+
+
+load( file="fitMCAR_MinPat_20PerMiss_4VarMiss_WM_fimlc_n1000.RData")
+load( file="fitMCAR_MinPat_20PerMiss_4VarMiss_WM_fimlc_compo_n1000.RData")
+fitMCAR_MinPat_20PerMiss_4VarMiss_WM_fimlc_checks_n1000<- 
+  fimlc.checks(fitMCAR_MinPat_20PerMiss_4VarMiss_WM_fimlc_compo_n1000,
+               fitMCAR_MinPat_20PerMiss_4VarMiss_WM_fimlc_n1000)
+
+save(fitMCAR_MinPat_20PerMiss_4VarMiss_WM_fimlc_checks_n1000, file="fitMCAR_MinPat_20PerMiss_4VarMiss_WM_fimlc_checks_n1000.RData")
+
+
+
+
+load( file="fitMCAR_MinPat_50PerMiss_4VarMiss_WM_fimlc_compo_n1000.RData")
+load(file="fitMCAR_MinPat_50PerMiss_4VarMiss_WM_fimlc_n1000.RData")
+fitMCAR_MinPat_50PerMiss_4VarMiss_WM_fimlc_checks_n1000<- 
+  fimlc.checks(fitMCAR_MinPat_50PerMiss_4VarMiss_WM_fimlc_compo_n1000,
+               fitMCAR_MinPat_50PerMiss_4VarMiss_WM_fimlc_n1000)
+
+
+save(fitMCAR_MinPat_50PerMiss_4VarMiss_WM_fimlc_checks_n1000, file="fitMCAR_MinPat_50PerMiss_4VarMiss_WM_fimlc_checks_n1000.RData")
+
+
+
+
+load( file="fitMCAR_MinPat_20PerMiss_6VarMiss_WM_fimlc_compo_n1000.RData")
+load( file="fitMCAR_MinPat_20PerMiss_6VarMiss_WM_fimlc_n1000.RData")
+fitMCAR_MinPat_20PerMiss_6VarMiss_WM_fimlc_checks_n1000<- 
+  fimlc.checks(fitMCAR_MinPat_20PerMiss_6VarMiss_WM_fimlc_compo_n1000,
+               fitMCAR_MinPat_20PerMiss_6VarMiss_WM_fimlc_n1000)
+
+save(fitMCAR_MinPat_20PerMiss_6VarMiss_WM_fimlc_checks_n1000, file="fitMCAR_MinPat_20PerMiss_6VarMiss_WM_fimlc_checks_n1000.RData")
+
+
+
+
+
+load(file="fitMCAR_MinPat_50PerMiss_6VarMiss_WM_fimlc_compo_n1000.RData")
+load(file="fitMCAR_MinPat_50PerMiss_6VarMiss_WM_fimlc_n1000.RData")
+fitMCAR_MinPat_50PerMiss_6VarMiss_WM_fimlc_checks_n1000<- 
+  fimlc.checks(fitMCAR_MinPat_50PerMiss_6VarMiss_WM_fimlc_compo_n1000,
+               fitMCAR_MinPat_50PerMiss_6VarMiss_WM_fimlc_n1000)
+
+
+save(fitMCAR_MinPat_50PerMiss_6VarMiss_WM_fimlc_checks_n1000, file="fitMCAR_MinPat_50PerMiss_6VarMiss_WM_fimlc_checks_n1000.RData")
+
+
+load(file="fitMCAR_MaxPat_20PerMiss_2VarMiss_WM_fimlc_compo_n1000.RData")
+load(file="fitMCAR_MaxPat_20PerMiss_2VarMiss_WM_fimlc_n1000.RData")
+fitMCAR_MaxPat_20PerMiss_2VarMiss_WM_fimlc_checks_n1000<- 
+  fimlc.checks(fitMCAR_MaxPat_20PerMiss_2VarMiss_WM_fimlc_compo_n1000,
+               fitMCAR_MaxPat_20PerMiss_2VarMiss_WM_fimlc_n1000)
+
+save(fitMCAR_MaxPat_20PerMiss_2VarMiss_WM_fimlc_checks_n1000, file="fitMCAR_MaxPat_20PerMiss_2VarMiss_WM_fimlc_checks_n1000.RData")
+
+
+
+load( file="fitMCAR_MaxPat_50PerMiss_2VarMiss_WM_fimlc_compo_n1000.RData")
+load( file="fitMCAR_MaxPat_50PerMiss_2VarMiss_WM_fimlc_n1000.RData")
+fitMCAR_MaxPat_50PerMiss_2VarMiss_WM_fimlc_checks_n1000<- 
+  fimlc.checks(fitMCAR_MaxPat_50PerMiss_2VarMiss_WM_fimlc_compo_n1000,fitMCAR_MaxPat_50PerMiss_2VarMiss_WM_fimlc_n1000 )
+
+save(fitMCAR_MaxPat_50PerMiss_2VarMiss_WM_fimlc_checks_n1000, file="fitMCAR_MaxPat_50PerMiss_2VarMiss_WM_fimlc_checks_n1000.RData")
+
+
+
+
+load( file="fitMCAR_MaxPat_20PerMiss_4VarMiss_WM_fimlc_compo_n1000.RData")
+load( file="fitMCAR_MaxPat_20PerMiss_4VarMiss_WM_fimlc_n1000.RData")
+fitMCAR_MaxPat_20PerMiss_4VarMiss_WM_fimlc_checks_n1000<- 
+  fimlc.checks(fitMCAR_MaxPat_20PerMiss_4VarMiss_WM_fimlc_compo_n1000,
+               fitMCAR_MaxPat_20PerMiss_4VarMiss_WM_fimlc_n1000)
+
+
+save(fitMCAR_MaxPat_20PerMiss_4VarMiss_WM_fimlc_checks_n1000, file="fitMCAR_MaxPat_20PerMiss_4VarMiss_WM_fimlc_checks_n1000.RData")
+
+
+load(file="fitMCAR_MaxPat_50PerMiss_4VarMiss_WM_fimlc_compo_n1000.RData")
+load( file="fitMCAR_MaxPat_50PerMiss_4VarMiss_WM_fimlc_n1000.RData")
+fitMCAR_MaxPat_50PerMiss_4VarMiss_WM_fimlc_checks_n1000<-
+  fimlc.checks(fitMCAR_MaxPat_50PerMiss_4VarMiss_WM_fimlc_compo_n1000,
+               fitMCAR_MaxPat_50PerMiss_4VarMiss_WM_fimlc_n1000)
+
+save(fitMCAR_MaxPat_50PerMiss_4VarMiss_WM_fimlc_checks_n1000, file="fitMCAR_MaxPat_50PerMiss_4VarMiss_WM_fimlc_checks_n1000.RData")
+
+
+
+
+load( file="fitMCAR_MaxPat_20PerMiss_6VarMiss_WM_fimlc_compo_n1000.RData")
+load( file="fitMCAR_MaxPat_20PerMiss_6VarMiss_WM_fimlc_n1000.RData")
+fitMCAR_MaxPat_20PerMiss_6VarMiss_WM_fimlc_checks_n1000<- 
+  fimlc.checks(fitMCAR_MaxPat_20PerMiss_6VarMiss_WM_fimlc_compo_n1000,
+               fitMCAR_MaxPat_20PerMiss_6VarMiss_WM_fimlc_n1000)
+
+save(fitMCAR_MaxPat_20PerMiss_6VarMiss_WM_fimlc_checks_n1000, file="fitMCAR_MaxPat_20PerMiss_6VarMiss_WM_fimlc_checks_n1000.RData")
+
+
+
+
+load(file="fitMCAR_MaxPat_50PerMiss_6VarMiss_WM_fimlc_compo_n1000.RData")
+load(file="fitMCAR_MaxPat_50PerMiss_6VarMiss_WM_fimlc_n1000.RData")
+fitMCAR_MaxPat_50PerMiss_6VarMiss_WM_fimlc_checks_n1000<- 
+  fimlc.checks(fitMCAR_MaxPat_50PerMiss_6VarMiss_WM_fimlc_compo_n1000,
+               fitMCAR_MaxPat_50PerMiss_6VarMiss_WM_fimlc_n1000)
+
+save(fitMCAR_MaxPat_50PerMiss_6VarMiss_WM_fimlc_checks_n1000, file="fitMCAR_MaxPat_50PerMiss_6VarMiss_WM_fimlc_checks_n1000.RData")
+
+
+
