d0222b4752788932ad9e2f79c932fa70b165d4ba
cathyxijuan <cathyxijuan@gmail.com>
2020-05-19 05:30:00 -0700

    delete

--- Simu results FIMLC/load FIMLC dif.R
+++
@@ -1,7 +0,0 @@
-setwd("/Volumes/SP PHD U3/missing-data-project-2/Simu results FIMLC")
-
-file.list <- list.files(path="FIMLC dif results", full.names=TRUE)
-
-for(i in 1:length(file.list)){
-  load(file.list[i])
-}
