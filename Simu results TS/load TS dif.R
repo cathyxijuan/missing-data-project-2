c038134638a89b1974f0a5af6cf77404565579cd
cathyxijuan <cathyxijuan@gmail.com>
2020-05-19 05:25:32 -0700

    delete

--- Simu results TS/load TS dif.R
+++
@@ -1,7 +0,0 @@
-setwd("/Volumes/SP PHD U3/missing-data-project-2/Simu results TS")
-
-file.list <- list.files(path="TS dif results", full.names=TRUE)
-
-for(i in 1:length(file.list)){
-  load(file.list[i])
-}
