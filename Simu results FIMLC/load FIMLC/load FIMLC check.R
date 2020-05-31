8fbc3ad8a902cfb638ee49235f20c645916435d8
cathyxijuan <cathyxijuan@gmail.com>
2020-05-30 21:30:18 -0700

    update

---
+++ Simu results FIMLC/load FIMLC/load FIMLC check.R
@@ -0,0 +1,9 @@
+setwd("/Volumes/SP PHD U3/missing-data-project-2/Simu results FIMLC")
+
+file.list <- list.files(path="FIMLC check results", full.names=TRUE)
+
+for(i in 1:length(file.list)){
+  load(file.list[i])
+}
+
+fitMCAR_50PerMiss_4VarMiss_2CR_SF_fimlc_checks_n200_mean
